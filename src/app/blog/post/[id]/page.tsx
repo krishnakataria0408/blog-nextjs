da14b2a54a6eea2f2723e37d0d37320e2afbd765
katariakrishna <kkataria@designcuts.com>
2024-12-24 00:08:35 +0000

    trying to solve error

--- src/app/blog/post/[id]/page.tsx
+++ src/app/blog/post/[id]/page.tsx
@@ -1,15 +1,20 @@
 import { posts } from '@/app/lib/placeholder-data';
 import Post from '@/app/ui/components/posts/Posts';
 
-export default function Page({ params }: { params: { id: string } }) {
-    const post = posts.find((post) => post.id === params.id);
-    if (!post) {
-        return <div>Post not found</div>;
-    }
+export default async function Page({ params }: { params: { id: string } }) {
+  
+  const { id } = await params;
 
-    return (
-      <>
-        <h1>Post</h1>
-        <Post {...post as { id: string; title: string; content: string; date: string }} />
-      </>)
+  const post = posts.find((post) => post.id === id);
+
+  if (!post) {
+    return <div>Post not found</div>;
   }
+
+  return (
+    <>
+      <h1>Post</h1>
+      <Post {...(post as { id: string; title: string; content: string; date: string })} />
+    </>
+  );
+}
